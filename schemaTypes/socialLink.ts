f8a46b3e4954f7bac70770892c72141efa25440b
Marcos Ramos <marcosperes.contato@gmail.com>
2025-05-08 11:46:30 -0300

    feat- updated social link schema to include social username

--- schemaTypes/socialLink.ts
+++ schemaTypes/socialLink.ts
@@ -5,14 +5,36 @@ export default defineType({
   title: 'Cadastro de Links Sociais',
   type: 'document',
   fields: [
-    defineField({ name: 'title', title: 'Titulo',description:"Descreva a rede social e a quem ela pertence, assim fica facil de encontrar quando necessário. ex.: instagram do Marcos", type: 'string', validation: Rule => Rule.required() }),
+    defineField({ name: 'title', 
+      title: 'Titulo',
+      description:"Descreva a rede social e a quem ela pertence, assim fica facil de encontrar quando necessário. ex.: instagram do Marcos", 
+      type: 'string', 
+      validation: Rule => Rule.required() 
+    }),
+    defineField({ 
+      name: 'socialUserName',
+      title: 'UserName da rede social',
+      description: "Nome do usuário, ou @, pra ser usado de forma textual quando necessário", 
+      type: 'string', 
+       validation: Rule => Rule.required()
+        .regex(/^[a-zA-Z0-9@#-_.]+$/, {
+          name: 'usernameformt',
+          invert: false,
+        })
+        .error('Use apenas letras maiuscula,minúsculas,números e os caracteres @ ou #, sem espaços. Ex: @MonteSiao, #TomandoPosseDaTerra'),
+    }),
+    
     defineField({ 
         name: 'type', 
         title: 'Tipo', 
         type: 'reference',
         to: [{ type: 'supportedSocialMidia' }], 
         validation: Rule => Rule.required() }),
-    defineField({ name: 'url', title: 'Link', type: 'url', validation: Rule => Rule.required() }),
+    defineField({ name: 'url', 
+      title: 'Link', 
+      type: 'url', 
+      validation: Rule => Rule.required() 
+    }),
 
   ],
 })
